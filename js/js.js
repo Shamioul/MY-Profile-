91b28b82d42f2b96b27b3cf73d9008fa0a209d88
MD Shamiul Islam <121965039+Shamioul@users.noreply.github.com>
2024-12-03 13:49:19 +0600

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$('.slick_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    arrows:true,
+    prevArrow:"<i class=Arrows><i class='Arrows fa-solid fa-arrow-left'></i></i>",
+    nextArrow:"<i class=Arrows2><i class='Arrows fa-solid fa-arrow-right'></i>",
+  });
+      
